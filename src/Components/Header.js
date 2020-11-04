e16467056dbf2cf258c1b9a746cc3e73963ebb62
vinitaran <vinitarane1998@gmail.com>
2020-11-04 22:11:17 +0530

    init

---
+++ src/Components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import "./Header.css";
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+                
+            </div>
+            <div className="header__right_1">
+                kard
+            </div>
+            <div className="header__right_2">
+                kardless
+            </div>
+            <div className="header__right_3">
+                security
+            </div>
+            <div className="header__right_4">
+                rewards
+            </div>
+            <div className="header__right_5">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/Components/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./Main.css";
+
+function Main() {
+    return (
+        <div className="body">
+            <div className="main">
+                <p>Kashware is an exclusive ecosystem that unites your money.</p>
+            </div>
+            <div className="content">
+                <p>#Spend #Save #StaySecured #EarnRewards #JoinTheKlub</p>
+            </div>
+            <div className="downArrow"></div>
+        </div>
+    )
+}
+
+export default Main
